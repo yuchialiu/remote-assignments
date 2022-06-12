20da75af91ec051d3b7a68cd1c5716ad3d8d76ac
yuchialiu <ellieliu128@gmail.com>
2022-06-12 19:59:46 +0800

    style.css commit

--- Week-1/Assignment-3/count.js
+++ Week-1/Assignment-3/count.js
@@ -1,1 +1,19 @@
+function countAandB(input) {
+// your code here
+  
+}
+function toNumber(input) {
+// your code here
+
+}
+let input1 = ['a', 'b', 'c', 'a', 'c', 'a', 'c'];
+console.log(countAandB(input1)); // should print 4 (3 ‘a’ letters and 1 ‘b’ letter)
+
+console.log(toNumber(input1); // should print [1, 2, 3, 1, 3, 1, 3]
+
+let input2 = ['e', 'd', 'c', 'd', 'e'];
+console.log(countAandB(input2)); // should print 0
+
+console.log(toNumber(input2)); // should print [5, 4, 3, 4, 5]
+
 
