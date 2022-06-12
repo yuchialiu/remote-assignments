5d29c517632561630fa066d980f8ab05355cfb8f
yuchialiu <ellieliu128@gmail.com>
2022-06-12 22:34:17 +0800

    Update count.js

--- Week-1/Assignment-3/count.js
+++ Week-1/Assignment-3/count.js
@@ -1,15 +1,30 @@
 function countAandB(input) {
 // your code here
-  
+	let count = 0;
+	for (i = 0; i < input.length; i++) {
+     if (input[i] === 'a' || input[i] === 'b') {
+      count++;
+    }
+  }
+  console.log(count)
 }
+
+
 function toNumber(input) {
 // your code here
-
+	var arr = []
+	for (i = 0; i < input.length; i++) {
+    console.log(input[i].charCodeAt(0)-96)
+    arr.push(input[i].charCodeAt(0)-96)
+  }
+  
+  return arr
 }
+
 let input1 = ['a', 'b', 'c', 'a', 'c', 'a', 'c'];
 console.log(countAandB(input1)); // should print 4 (3 ‘a’ letters and 1 ‘b’ letter)
 
-console.log(toNumber(input1); // should print [1, 2, 3, 1, 3, 1, 3]
+console.log(toNumber(input1)); // should print [1, 2, 3, 1, 3, 1, 3]
 
 let input2 = ['e', 'd', 'c', 'd', 'e'];
 console.log(countAandB(input2)); // should print 0
